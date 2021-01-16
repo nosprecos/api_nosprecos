ec22cf5149d59775c6933609f6c5f65f50ff4bce
Ariel Carvalho <arielwsc17@gmail.com>
2021-01-15 23:59:39 -0600

    Started Postgre connection

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "nosPrecos",
+      user: "postgres",
+      password: "010203"
+    }
+  }
+
+};
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.listen(7777, () => console.log('Server is running'))
+
