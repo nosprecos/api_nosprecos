3b2a025b46d7e68f42478402af4dbc5d1d5230c6
mathvra <matheusvram@gmail.com>
2021-02-15 00:12:42 -0300

    correction in error phrases

--- src/error-script.js
+++ src/error-script.js
@@ -1,25 +1,25 @@
 module.exports = {
     no_username: 'Login de usuário nao informado',
-    min_char_user: 'Login de usuário não atinge numero de char mínimo',
+    min_char_user: 'Login de usuário não atinge número de char mínimo',
     max_char_user: 'Login de usuário passou do valor máximo',
-    existing_username: 'Login de usuario ja utilizado',
+    existing_username: 'Login de usuário ja utilizado',
 
-    no_realname: 'Nome de usuario nao informado',
+    no_realname: 'Nome de usuário nao informado',
     max_char_name: 'Nome de usuário passou do valor máximo',
 
-    no_password: 'Senha de usuario nao informado',
-    no_confirm_pass: 'Senha de confirmacao de usuario nao informado',
+    no_password: 'Senha de usuário nao informado',
+    no_confirm_pass: 'Senha de confirmacao de usuário nao informado',
     mismatch_password: 'Senhas não são iguais',
-    not_secured_password: 'Senha nao esta conforme nivel de seguranca',
-    wrong_password: 'Senha de usuario esta incorreta',
+    not_secured_password: 'Senha não está conforme nivel de segurança',
+    wrong_password: 'Senha de usuário está incorreta',
 
-    no_email: 'Email de usuario nao informado',
-    existing_email: 'Email de usuario ja utilizado',
-    invalid_email: 'Email de usuario invalido',
+    no_email: 'Email de usuário não informado',
+    existing_email: 'Email de usuário já utilizado',
+    invalid_email: 'Email de usuário inválido',
 
-    cant_find_customer: 'Nao foi possivel encontrar customer',
+    cant_find_customer: 'Não foi possível encontrar customer',
     login_failed: 'Usuário ou senha incorreta',
-    no_login_name: 'Nome ou email de usuario nao informado',
+    no_login_name: 'Nome ou email de usuário não informado',
 
     length_id: 'Tamanho do ID informado incorreto',
 
